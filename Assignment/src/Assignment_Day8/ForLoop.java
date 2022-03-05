084f1f82c173af8ae89f37854d089326d2ff06b4
DELL <DELL@DESKTOP-VDVJS3C>
2022-03-05 19:04:57 +0530

    Pushing new folder

---
+++ Assignment/src/Assignment_Day8/ForLoop.java
@@ -0,0 +1,12 @@
+package Assignment_Day8;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++) {
+			System.out.println("My Name is Sahil Sharma");
+		}
+
+	}
+
+}
